d1ded5cd7a87299c0b80a9ba38e5570a735188b8
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2015-10-24 16:31:45 +0400

    Refactoring

--- OpenCVTutorial/main.cpp
+++ OpenCVTutorial/main.cpp
@@ -23,8 +23,12 @@
 
 static const std::string kFrameTitleRGB = "RGB Window";
 static const std::string kFrameTitleHSV = "HSV Window";
-static const std::string kFRameTitleHSVFiltered = "Filtered HSV Window";
+static const std::string kFrameTitleHSVFiltered = "Filtered HSV Window";
 static const std::string kFrameTitleHSVFilterControls = "HSV Filter Controls";
+
+static const int kIDWebcam = 0;
+static const int kFrameWidth = 600;
+static const int kFrameHeight = 400;
 
 static int hueMinimum = 0;
 static int hueMaximum = 256;
@@ -34,10 +38,6 @@ static int saturationMaximum = 256;
 
 static int valueMinimum = 156;
 static int valueMaximum = 256;
-
-static const int kIDWebcam = 0;
-static const int kFrameWidth = 600;
-static const int kFrameHeight = 400;
 
 void on_trackbar( int, void* )
 {
@@ -50,24 +50,14 @@ void createTrackers(){
     
     cv::namedWindow(kFrameTitleHSVFilterControls,0);
     
-    char TrackbarName[50];
-    sprintf( TrackbarName, "H_MIN", hueMinimum);
-    sprintf( TrackbarName, "H_MAX", hueMaximum);
-    sprintf( TrackbarName, "S_MIN", saturationMinimum);
-    sprintf( TrackbarName, "S_MAX", saturationMaximum);
-    sprintf( TrackbarName, "V_MIN", valueMinimum);
-    sprintf( TrackbarName, "V_MAX", valueMaximum);
-    
     cv::createTrackbar("H Min", kFrameTitleHSVFilterControls, &hueMinimum, hueMaximum, on_trackbar);
-    cv::createTrackbar("H Max", kFrameTitleHSVFilterControls, &hueMaximum, hueMaximum,
-        on_trackbar);
+    cv::createTrackbar("H Max", kFrameTitleHSVFilterControls, &hueMaximum, hueMaximum, on_trackbar);
     
     cv::createTrackbar("S MIN", kFrameTitleHSVFilterControls, &saturationMinimum, saturationMaximum, on_trackbar);
     cv::createTrackbar("S MAX", kFrameTitleHSVFilterControls, &saturationMaximum, saturationMaximum, on_trackbar);
     
     cv::createTrackbar("V MIN", kFrameTitleHSVFilterControls, &valueMinimum, valueMaximum, on_trackbar);
-    cv::createTrackbar("V MAX", kFrameTitleHSVFilterControls, &valueMaximum, valueMaximum,
-                       on_trackbar);
+    cv::createTrackbar("V MAX", kFrameTitleHSVFilterControls, &valueMaximum, valueMaximum, on_trackbar);
     
 }
 
@@ -82,8 +72,8 @@ void executeMorphologicalOperations(cv::Mat& inputOutputMatrix){
     cv::Mat dilateMatrix = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(8,8));
     
     //Erode and Dilate can be called multiple times if necessary (TODO: Check Why? Seems Strange!)
-    cv::erode(inputOutputMatrix, inputOutputMatrix, erodeMatrix);
-    cv::dilate(inputOutputMatrix, inputOutputMatrix, dilateMatrix);
+    cv::erode(/* IN */ inputOutputMatrix, /* OUT */ inputOutputMatrix, erodeMatrix);
+    cv::dilate(/* IN */ inputOutputMatrix, /* OUT */ inputOutputMatrix, dilateMatrix);
 }
 
 int main(int argc, const char * argv[]) {
@@ -98,7 +88,7 @@ int main(int argc, const char * argv[]) {
     
     cv::Mat rgbMatrix;
     cv::Mat hsvMatrix;
-    cv::Mat hsvMatrixFiltered;
+    cv::Mat hsvFilteredMatrix;
 
     while (true) {
         videoCapture.read(rgbMatrix);
@@ -107,15 +97,15 @@ int main(int argc, const char * argv[]) {
         cv::inRange(hsvMatrix,
                     cv::Scalar(hueMinimum,saturationMinimum,valueMinimum),
                     cv::Scalar(hueMaximum,saturationMaximum,valueMaximum),
-                    hsvMatrixFiltered);
+                    hsvFilteredMatrix);
         
         //std::cout<<"H: "<<hueMinimum<<" S: "<<saturationMinimum<<" V: "<<valueMinimum;
         //std::cout<<" - ";
         //std::cout<<"H: "<<hueMaximum<<" S: "<<saturationMaximum<<" V: "<<valueMaximum<<"\n";
         
-        executeMorphologicalOperations(hsvMatrixFiltered);
+        executeMorphologicalOperations(hsvFilteredMatrix);
         
-        cv::imshow(kFRameTitleHSVFiltered, hsvMatrixFiltered);
+        cv::imshow(kFrameTitleHSVFiltered, hsvFilteredMatrix);
         cv::imshow(kFrameTitleRGB, rgbMatrix);
         cv::imshow(kFrameTitleHSV, hsvMatrix);
         
